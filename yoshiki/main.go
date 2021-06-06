5dbbc20d4eeb5306fb760bc448cb24009d918414
face2Sato <main8102000@gmail.com>
2021-06-06 17:45:54 +0900

    main.go was modified

--- yoshiki/main.go
+++ yoshiki/main.go
@@ -3,5 +3,52 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello World!\n")
+
+	/*var unko [3]string = [3]string{"unko", "pee", "poop"}
+
+	unko := [3]string{"syamu", "pee", "poop"}
+
+	for _, v := range unko {
+		fmt.Println("value = " + v)
+		fmt.Println(FindGarbage(v))
+	}*/
+
+	num := [11]int{0, 14, 22, 2, 20, 5, 211, 7, 8, 90, 50}
+	/*
+		for _, v := range num {
+			a, b := oddeven(&v)
+			fmt.Println(b)
+
+			if a {
+				fmt.Printf("hoge\n")
+			} else {
+				fmt.Printf("hage\n")
+			}
+		}
+	*/
+	reverse(num, len(num))
+}
+
+func reverse(a [11]int, length int) {
+
+	for i := 0; i < length; i++ {
+		defer fmt.Println(a[i])
+	}
+
+}
+
+func oddeven(a *int) (bool, string) {
+	if *a%2 != 0 {
+		return false, "even"
+	} else {
+		return true, "odd"
+	}
+}
+
+func FindGarbage(v string) string {
+	if v == "syamu" {
+		return "garbage"
+	} else {
+		return "go to toilet"
+	}
 }
