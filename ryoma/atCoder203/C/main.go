560a69f5edf3d2db53ba744b4e0d3f91249e8d8b
ryoma okuda <catontheskate4423@gmail.com>
2021-06-08 15:20:21 +0900

    atcoder C ryoma ok

--- ryoma/atCoder203/C/main.go
+++ ryoma/atCoder203/C/main.go
@@ -1,66 +1,66 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 var (
-	N       int
-	K       int
-	A       []int
-	B       []int
-	C       []bool
-	answer  int
-	current int
+	N          int
+	K          int
+	_tempArray [][]int
+	answer     int
+	current    int
 )
 
 func main() {
-	// データを取ってくる
-	fmt.Scanf("%d %d", &N, &K)
-	for i := 0; i < N; i++ {
-		_a := 0
-		_b := 0
-		fmt.Scanf("%d %d", &_a, &_b)
-		A = append(A, _a)
-		B = append(B, _b)
-		C = append(C, false)
-	}
-	//　実行
-	answer = run()
+	// - fetch data
+	var sc = bufio.NewScanner(os.Stdin)
 
+	// N,K
+	if sc.Scan() {
+		_tmp := strings.Fields(sc.Text())
+		N = unwrapAtoi(_tmp[0])
+		K = unwrapAtoi(_tmp[1])
+	}
+
+	for i := 0; i < N; i++ {
+		_tempArray = append(_tempArray, []int{})
+		if sc.Scan() {
+			_tmp := strings.Fields(sc.Text())
+			_tempArray[i] = append(_tempArray[i], unwrapAtoi(_tmp[0]))
+			_tempArray[i] = append(_tempArray[i], unwrapAtoi(_tmp[1]))
+		}
+	}
+	// multiplications
+	answer = run()
 	fmt.Println(answer)
 }
 
 func run() int {
-	// お金をもらう予定の人数を入れる
-	road := len(A)
+	// sorting stuff
+	sort.Slice(_tempArray, func(i, j int) bool { return _tempArray[i][0] < _tempArray[j][0] })
 
-	for {
-		min := math.MaxInt32
-		for i, s := range A {
-			if C[i] == false && min > s {
-				min = s
-			}
-		}
-
-		for j, s := range A {
-			if s == min {
-				if K < s-current {
-					return current + K
-				} else {
-					// go to the village to get the monimoni
-					K -= s - current
-					current += s - current
-					// get monimoni
-					K += B[j]
-				}
-				C[j] = true
-				road--
-			}
-		}
-		if road == 0 {
+	// every array multiplications
+	for j, s := range _tempArray {
+		if K < s[0]-current {
 			return current + K
+		} else {
+			// go to the village to get the monimoni
+			K -= s[0] - current
+			current += s[0] - current
+			// get monimoni
+			K += _tempArray[j][1]
 		}
 	}
+	return current + K
+}
+
+func unwrapAtoi(word string) int {
+	_tmp, _ := strconv.Atoi(word)
+	return _tmp
 }
